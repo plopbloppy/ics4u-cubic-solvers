2ffff19440d1ffdf2e4f971e953efd5b8f5e1762
plopbloppy <kaili.tang2@gmail.com>
2026-03-10 11:32:17 -0400

    Fixed drawing roots loop in drawFunction function.

--- assignment-two/script.ts
+++ assignment-two/script.ts
@@ -5,12 +5,6 @@ const centerX = canvas.width / 2 + 5;
 const centerY = canvas.height / 2 + 5;
 const gridSize = 20;
 
-//roots
-let x1: any;
-let x2: any;
-let x3: any;
-
-//grid
 function drawGrid() {
     if (ctx) {
         ctx.beginPath();
@@ -29,16 +23,13 @@ function drawGrid() {
         ctx.lineTo(canvas.width, centerY);
         ctx.moveTo(centerX, 5);
         ctx.lineTo(centerX, canvas.height);
-
         ctx.strokeStyle = "#3c8dbc";
         ctx.lineWidth = 2;
         ctx.stroke();
     }
 }
 
-function drawFunction(a: number, b: number, c: number, d: number, x1: any, x2: any, x3: any) {
-    const roots = [x1, x2, x3];
-
+function drawFunction(a: number, b: number, c: number, d: number, roots: any[]) {
     if (ctx) {
         ctx.translate(centerX, centerY);
         ctx.beginPath();
@@ -54,6 +45,7 @@ function drawFunction(a: number, b: number, c: number, d: number, x1: any, x2: a
 
         ctx.beginPath();
 
+        //draws a dot around each real root
         for (let i = 0; i < roots.length; i++) {
             ctx.moveTo(roots[i] * gridSize, 0);
             ctx.arc(roots[i] * gridSize, 0, 3, 0, 2 * Math.PI);
@@ -64,12 +56,12 @@ function drawFunction(a: number, b: number, c: number, d: number, x1: any, x2: a
     }
 }
 
-function updateGraph(a: number, b: number, c: number, d: number, x1: any, x2: any, x3: any) {
+function updateGraph(a: number, b: number, c: number, d: number, roots: any[]) {
     if (ctx) {
         ctx.setTransform(1, 0, 0, 1, 0, 0);
         ctx.clearRect(0, 0, canvas.width, canvas.height);
         drawGrid();
-        drawFunction(a, b, c, d, x1, x2, x3);
+        drawFunction(a, b, c, d, roots);
     }
 }
 
@@ -89,36 +81,51 @@ form?.addEventListener("submit", (event) => {
     const q = (27 * a * a * d - 9 * a * b * c + 2 * b ** 3) / (27 * a ** 3);
     const discriminant = (q / 2) ** 2 + (p / 3) ** 3;
 
+    let x1: any;
+    let x2: any;
+    let x3: any;
+    let roots: number[];
+
     if (discriminant < 0) {
+        //three disctinct roots
         const k = 2 * Math.sqrt(-p / 3);
         const theta = Math.acos(-q / (2 * Math.sqrt(-1 * ((p / 3) ** 3)))) / 3;
         x1 = k * Math.cos(theta) - b / (3 * a);
         x2 = k * Math.cos(theta + (2 * Math.PI) / 3) - b / (3 * a);
         x3 = k * Math.cos(theta + (4 * Math.PI / 3)) - b / (3 * a);
+        roots = [x1, x2, x3];
 
+        //delete this line once done testing
         (document.getElementById("result") as HTMLInputElement).value = `x1=${x1}, x2=${x2}, x3=${x3}`;
     } else if (discriminant > 0) {
-        const U = -q / 2 + Math.sqrt((q / 2) ** 2 + (p / 3) ** 3);
-        const V = -q / 2 - Math.sqrt((q / 2) ** 2 + (p / 3) ** 3);
-        x1 = U ** (1 / 3);
-        x2 = V ** (1 / 3);
+        x1 = (-q / 2 + Math.sqrt((q / 2) ** 2 + (p / 3) ** 3)) ** (1 / 3);
+        x2 = (-q / 2 - Math.sqrt((q / 2) ** 2 + (p / 3) ** 3)) ** (1 / 3);
         x3 = x1 + x2 - b / (3 * a);
+        roots = [x3];
 
-        (document.getElementById("result") as HTMLInputElement).value = `x1= Complex Number, x2= Complex Number, x3=${x3}`;
+        (document.getElementById("x1") as HTMLInputElement).textContent = `${x1}`;
     } else {
         if (p == q && p == 0) {
-            const x1 = (-q / 2 + Math.sqrt((q / 2) ** 2 + (p / 3) ** 3)) ** 2 / 3 - b / (3 * a);
-            const x2 = (-q / 2 + Math.sqrt((q / 2) ** 2 + (p / 3) ** 3)) ** 2 / 3 - b / (3 * a);
-            const x3 = (-q / 2 + Math.sqrt((q / 2) ** 2 + (p / 3) ** 3)) ** 2 / 3 - b / (3 * a);
-            (document.getElementById("result") as HTMLInputElement).value = `x1=${x1}, x2=${x2}, x3=${x3}`;
+            x1 = (-q / 2 + Math.sqrt((q / 2) ** 2 + (p / 3) ** 3)) ** 2 / 3 - b / (3 * a);
+            x2 = x1;
+            x3 = x1;
+            roots = [x1];
 
+            (document.getElementById("result") as HTMLInputElement).value = `x1=${x1}, x2=${x2}, x3=${x3}`;
         } else {
-            const x1 = (p / 2) ** 1 / 3 - b / (3 * a);
-            const x2 = (p / 2) ** 1 / 3 - b / (3 * a);
-            const x3 = -2 * x1;
+            x1 = (p / 2) ** 1 / 3 - b / (3 * a);
+            x2 = (p / 2) ** 1 / 3 - b / (3 * a);
+            x3 = -2 * x1;
+            roots = [x1, x2, x3];
 
             (document.getElementById("result") as HTMLInputElement).value = `x1=${x1}, x2=${x2}, x3=${x3}`;
         }
     }
-    updateGraph(a, b, c, d, x1, x2, x3);
+    // (document.getElementById("p") as HTMLInputElement).textContent = `${p}`;
+    // (document.getElementById("discriminant") as HTMLInputElement).textContent = `${discriminant}`;
+    // (document.getElementById("x1") as HTMLInputElement).textContent = `${x1}`;
+    // (document.getElementById("x2") as HTMLInputElement).textContent = `${x2}`;
+    // (document.getElementById("x3") as HTMLInputElement).textContent = `${x3}`;
+
+    updateGraph(a, b, c, d, roots);
 })
